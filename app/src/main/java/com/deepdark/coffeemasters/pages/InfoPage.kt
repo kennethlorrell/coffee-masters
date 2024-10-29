1894256025b7059592255510e5081e5dfe5ef2d2
Max <39443507+kennethlorrell@users.noreply.github.com>
2024-10-29 06:45:21 +0200

    Add info page as web view

--- app/src/main/java/com/deepdark/coffeemasters/pages/InfoPage.kt
+++ app/src/main/java/com/deepdark/coffeemasters/pages/InfoPage.kt
@@ -1,10 +1,31 @@
 package com.deepdark.coffeemasters.pages
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun MyWebView() {
+    val url = "https://firtman.github.io/coffeemasters/webapp"
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = { webView ->
+        webView.loadUrl(url)
+    })
+}
 
 @Preview
 @Composable
 fun InfoPage() {
-
+    MyWebView()
 }
